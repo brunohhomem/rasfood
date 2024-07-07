bce3c9eb9eb94730566574380b81495962eb231a
Bruno H. Homem <brunohhomem@gmail.com>
2024-07-07 14:42:59 -0300

    feat: added pratoDao

---
+++ src/main/java/br/com/rasmoo/restaurant/dao/PratoDao.java
@@ -0,0 +1,19 @@
+package br.com.rasmoo.restaurant.dao;
+
+import br.com.rasmoo.restaurant.entity.Prato;
+
+import javax.persistence.EntityManager;
+
+public class PratoDao {
+
+    private EntityManager entityManager;
+
+    public PratoDao(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
+    public void cadastrar (Prato prato){
+        this.entityManager.persist(prato);
+        System.out.println("Entidade cadastrada com sucesso! " + prato);
+    }
+}
