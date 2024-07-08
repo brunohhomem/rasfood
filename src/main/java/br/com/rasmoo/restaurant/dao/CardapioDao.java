778e86771da6b7777ebf2f7c9f94eeae5e6a5b9b
Bruno H. Homem <brunohhomem@gmail.com>
2024-07-08 09:46:03 -0300

    feat: added cardapio > consultarTodos

--- src/main/java/br/com/rasmoo/restaurant/dao/CardapioDao.java
+++ src/main/java/br/com/rasmoo/restaurant/dao/CardapioDao.java
@@ -3,6 +3,7 @@ package br.com.rasmoo.restaurant.dao;
 import br.com.rasmoo.restaurant.entity.Cardapio;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class CardapioDao {
 
@@ -17,8 +18,13 @@ public class CardapioDao {
         System.out.println("Entidade cadastrada com sucesso! " + cardapio);
     }
 
-    public Cardapio consultar(final Integer id) {
+    public Cardapio consultarPorId(final Integer id) {
         return this.entityManager.find(Cardapio.class, id);
+    }
+
+    public List<Cardapio> consultarTodos() {
+        String sql = "SELECT c FROM Cardapio c";
+        return this.entityManager.createQuery(sql, Cardapio.class).getResultList();
     }
 
     public void atualizar(final Cardapio cardapio) {
